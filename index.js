0bf69b95777e3500acb94f59d866e659cf814b2f
kapilkarda <kkarda77@gmail.com>
2022-08-16 16:54:00 +0530

    changes in reset password

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
+const {recurringPayment} = require('./src/controllers/companyAdmin.conroller')
+
 const app = express();
 
 const Router = require('./src/routes/index');
@@ -11,6 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'))
+
+// cron.schedule('59 59 23 * * *', async () => {
+//     await recurringPaymentCron();
+//   });
+//   cronJob.start();
+
 
 app.listen(process.env.LISTEN_PORT, () => {
     console.log(`Hirise sales is running on ${process.env.LISTEN_PORT} `);
