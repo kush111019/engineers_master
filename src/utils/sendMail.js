29473a38f3abb52b6c787d8d9c99d0f48dbb36b9
kapilkarda <kkarda77@gmail.com>
2022-09-06 12:45:32 +0530

    changed email send functionality from nodemailer to sendInBlue

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,217 +1,95 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require('sib-api-v3-sdk');
 const welcomeTemplate = require('../templates/welcome')
 const resetPassTemplate = require('../templates/resetPassword')
 const setPassTemp = require('../templates/setPassword')
 const contactUsTemplate = require('../templates/contactUs')
 require('dotenv').config()
 
-// module.exports.sendEmail = async (email,code) => {
-//     const smtpEndpoint = "smtp.gmail.com";
-//     const port = 587;
-//     const senderAddress = process.env.SMTP_USERNAME;
-//     var toAddresses = email;
-
-//     var ccAddresses = "";
-//     var bccAddresses = "";
-
-//     const smtpUsername = process.env.SMTP_USERNAME;
-//     const smtpPassword = process.env.SMTP_PASSWORD;
-
-//     // The subject line of the email
-//     var subject = "Admin verification";
-//     // The email body for recipients with non-HTML email clients.
-//     var body_text = `Please use the below otp code to verify your self`;
-
-//     // The body of the email for recipients whose email clients support HTML contenty.
-//     //var body_html= emailTem;
-//     var body_html = `<html>
-//                      <head></head>
-//                      <body>
-//                      <b> your verification code is :- ${code} </b>
-//                      </body>
-//                      </html>`;
-
-//     let transporter = nodemailer.createTransport({
-//         host: smtpEndpoint,
-//         port: port,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: smtpUsername,
-//             pass: smtpPassword
-//         }
-//     });
-
-//     // Specify the fields in the email.
-//     let mailOptions = {
-//         from: senderAddress,
-//         to: toAddresses,
-//         subject: subject,
-//         cc: ccAddresses,
-//         bcc: bccAddresses,
-//         text: body_text,
-//         html: body_html,
-//         // Custom headers for configuration set and message tags.
-//         headers: {}
-//     };
-
-//     // Send the email.
-//     let info = await transporter.sendMail(mailOptions)
-//     console.log("Message sent! Message ID: ", info.messageId);
-
-// }
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+//Instantiate the client\
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.API_KEY;
 
 module.exports.welcomeEmail = async (email , link, userName) => {
-    const smtpEndpoint = "smtp.gmail.com";
-    const port = 587;
-    const senderAddress = process.env.SMTP_USERNAME;
-    var toAddresses = email;
 
-    let welcomeTemp = welcomeTemplate.welcome(link, userName)
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    //Define the campaign settings\
+    sendSmtpEmail.name = "Email sent via the API";
+    sendSmtpEmail.subject = "Welcome to hirise";
+    sendSmtpEmail.sender = { "name": "Hirise Tech", "email": process.env.SMTP_EMAIL };
+    sendSmtpEmail.type = "classic";
+    //Content that will be sent
+    sendSmtpEmail.htmlContent = welcomeTemplate.welcome(link, userName)
+    //Select the recipients
+    sendSmtpEmail.to = [{ "email" : email }]
+    //Schedule the sending in one hour
+    //scheduledAt = '2018-01-01 00:00:01'
 
-    var ccAddresses = "";
-    var bccAddresses = "";
-
-    const smtpUsername = process.env.SMTP_USERNAME;
-    const smtpPassword = process.env.SMTP_PASSWORD;
-
-    // The subject line of the email
-    var subject = "Welcome to HiRise";
-    // The email body for recipients with non-HTML email clients.
-    var body_text = `Please use the below link to activate your account and reset your password`;
-    
-    // The body of the email for recipients whose email clients support HTML contenty.
-    //var body_html= emailTem;
-
-    let transporter = nodemailer.createTransport({
-        host: smtpEndpoint,
-        port: port,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: smtpUsername,
-            pass: smtpPassword
-        }
-    });
-
-    // Specify the fields in the email.
-    let mailOptions = {
-        from: senderAddress,
-        to: toAddresses,
-        subject: subject,
-        cc: ccAddresses,
-        bcc: bccAddresses,
-        text: body_text,
-        html: welcomeTemp,
-        // Custom headers for configuration set and message tags.
-        headers: {}
-    };
-
-    // Send the email.
-    let info = await transporter.sendMail(mailOptions)
-    console.log("Message sent! Message ID: ", info.messageId);
-
+    //Make the call to the client
+    let sentdata = apiInstance.sendTransacEmail(sendSmtpEmail).then((data)=> {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        return JSON.stringify(data)
+      }).catch((error)=> {
+        console.error(error);
+        return error
+      });
+    return sentdata
 }
 
 module.exports.resetPasswordMail = async (email , link , userName) => {
-    const smtpEndpoint = "smtp.gmail.com";
-    const port = 587;
-    const senderAddress = process.env.SMTP_USERNAME;
-    var toAddresses = email;
 
-    let resetPass = resetPassTemplate.resetPassword(link, email, userName)
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    //Define the campaign settings\
+    sendSmtpEmail.name = "Email sent via the API";
+    sendSmtpEmail.subject = "Reset Password";
+    sendSmtpEmail.sender = { "name": "Hirise Tech", "email": process.env.SMTP_EMAIL };
+    sendSmtpEmail.type = "classic";
+    //Content that will be sent
+    sendSmtpEmail.htmlContent = resetPassTemplate.resetPassword(link, email, userName)
+    //Select the recipients
+    sendSmtpEmail.to = [{ "email" : email }]
+    //Schedule the sending in one hour
+    //scheduledAt = '2018-01-01 00:00:01'
 
-    var ccAddresses = "";
-    var bccAddresses = "";
-
-    const smtpUsername = process.env.SMTP_USERNAME;
-    const smtpPassword = process.env.SMTP_PASSWORD;
-
-    // The subject line of the email
-    var subject = "Reset password";
-    // The email body for recipients with non-HTML email clients.
-    var body_text = `Please use the below link to reset your password`;
-    
-    // The body of the email for recipients whose email clients support HTML contenty.
-    //var body_html= emailTem;
-
-    let transporter = nodemailer.createTransport({
-        host: smtpEndpoint,
-        port: port,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: smtpUsername,
-            pass: smtpPassword
-        }
-    });
-
-    // Specify the fields in the email.
-    let mailOptions = {
-        from: senderAddress,
-        to: toAddresses,
-        subject: subject,
-        cc: ccAddresses,
-        bcc: bccAddresses,
-        text: body_text,
-        html: resetPass,
-        // Custom headers for configuration set and message tags.
-        headers: {}
-    };
-
-    // Send the email.
-    let info = await transporter.sendMail(mailOptions)
-    console.log("Message sent! Message ID: ", info.messageId);
-
+    //Make the call to the client
+    let sentdata = apiInstance.sendTransacEmail(sendSmtpEmail).then((data)=> {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        return JSON.stringify(data)
+      }).catch((error)=> {
+        console.error(error);
+        return error
+      });
+    return sentdata
 }
 
 
 module.exports.setPasswordMail = async (email , link, userName) => {
-    const smtpEndpoint = "smtp.gmail.com";
-    const port = 587;
-    const senderAddress = process.env.SMTP_USERNAME;
-    var toAddresses = email;
 
-    let setPass = setPassTemp.setPassword(link, userName)
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    //Define the campaign settings\
+    sendSmtpEmail.name = "Email sent via the API";
+    sendSmtpEmail.subject = "Set password mail";
+    sendSmtpEmail.sender = { "name": "Hirise Tech", "email": process.env.SMTP_EMAIL };
+    sendSmtpEmail.type = "classic";
+    //Content that will be sent
+    sendSmtpEmail.htmlContent = setPassTemp.setPassword(link, userName)
+    //Select the recipients
+    sendSmtpEmail.to = [{ "email" : email }]
+    //Schedule the sending in one hour
+    //scheduledAt = '2018-01-01 00:00:01'
 
-    var ccAddresses = "";
-    var bccAddresses = "";
-
-    const smtpUsername = process.env.SMTP_USERNAME;
-    const smtpPassword = process.env.SMTP_PASSWORD;
-
-    // The subject line of the email
-    var subject = "Verification Mail";
-    // The email body for recipients with non-HTML email clients.
-    var body_text = `Please use the below link for verification`;
-    
-    // The body of the email for recipients whose email clients support HTML contenty.
-    //var body_html= emailTem;
-
-    let transporter = nodemailer.createTransport({
-        host: smtpEndpoint,
-        port: port,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: smtpUsername,
-            pass: smtpPassword
-        }
-    });
-
-    // Specify the fields in the email.
-    let mailOptions = {
-        from: senderAddress,
-        to: toAddresses,
-        subject: subject,
-        cc: ccAddresses,
-        bcc: bccAddresses,
-        text: body_text,
-        html: setPass,
-        // Custom headers for configuration set and message tags.
-        headers: {}
-    };
-
-    // Send the email.
-    let info = await transporter.sendMail(mailOptions)
-    console.log("Message sent! Message ID: ", info.messageId);
-
+    //Make the call to the client
+    let sentdata = apiInstance.sendTransacEmail(sendSmtpEmail).then((data)=> {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        return JSON.stringify(data)
+      }).catch((error)=> {
+        console.error(error);
+        return error
+      });
+    return sentdata
 }
 
 
@@ -266,53 +144,35 @@ module.exports.recurringPaymentMail = async (email,customerName) => {
 
 }
 
-module.exports.contactUsMail = async (email,fullName,subject,message,address) => {
-    const smtpEndpoint = "smtp.gmail.com";
-    const port = 587;
-    const senderAddress = process.env.SMTP_USERNAME;
-    var toAddresses = process.env.SMTP_USERNAME;
 
-    let contact = contactUsTemplate.contactUs(email,fullName,subject,message,address)
+module.exports.contactUsMail = async (email,fullName,subject1,message,address) => {
 
-    var ccAddresses = "";
-    var bccAddresses = "";
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    //Define the campaign settings\
+    sendSmtpEmail.name = "Email sent via the API";
+    sendSmtpEmail.subject = subject1;
+    sendSmtpEmail.sender = { "name": "Hirise Tech", "email": process.env.SMTP_EMAIL };
+    sendSmtpEmail.type = "classic";
+    //Content that will be sent
+    sendSmtpEmail.htmlContent = contactUsTemplate.contactUs(email,fullName,subject1,message,address)
+    //Select the recipients
+    sendSmtpEmail.to = [{ "email" : process.env.SMTP_EMAIL }]
+    //Schedule the sending in one hour
+    //scheduledAt = '2018-01-01 00:00:01'
 
-    const smtpUsername = process.env.SMTP_USERNAME;
-    const smtpPassword = process.env.SMTP_PASSWORD;
-
-    // The subject line of the email
-    //let subject = subject;
-    // The email body for recipients with non-HTML email clients.
-    var body_text = `Contact us Mail`;
-    
-    // The body of the email for recipients whose email clients support HTML contenty.
-    //var body_html= emailTem;
-
-    let transporter = nodemailer.createTransport({
-        host: smtpEndpoint,
-        port: port,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: smtpUsername,
-            pass: smtpPassword
-        }
-    });
-
-    // Specify the fields in the email.
-    let mailOptions = {
-        from: senderAddress,
-        to: toAddresses,
-        subject: subject,
-        cc: ccAddresses,
-        bcc: bccAddresses,
-        text: body_text,
-        html: contact,
-        // Custom headers for configuration set and message tags.
-        headers: {}
-    };
-
-    // Send the email.
-    let info = await transporter.sendMail(mailOptions)
-    console.log("Message sent! Message ID: ", info.messageId);
-
+    //Make the call to the client
+    let sentdata = apiInstance.sendTransacEmail(sendSmtpEmail).then((data)=> {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        return JSON.stringify(data)
+      }).catch((error)=> {
+        console.error(error);
+        return error
+      });
+    return sentdata
 }
+//---------------------------------------------------------------------------------------
+
+
+
+
