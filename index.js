580b685e210b1149e49e5fe403916899c20244b5
kapilkarda <kkarda77@gmail.com>
2022-10-14 16:34:28 +0530

    removed console in index.js

--- index.js
+++ index.js
@@ -55,11 +55,9 @@ io.on("connection", (socket) => {
   socket.on("new message", (newMessageRecieved) => {
     // var chat = newMessageRecieved.chat;
 
-    console.log(newMessageRecieved,"new message recieved");
     if (!newMessageRecieved.users) return console.log("chat.users not defined");
 
     newMessageRecieved.users.forEach((user) => {
-      console.log(user, "send to user");
       if (user.id == newMessageRecieved.sender.id) return;
 
       socket.in(user.id).emit("message recieved", newMessageRecieved);
