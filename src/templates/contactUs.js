8929bc9ae2de913184b98a5d6b9fbd16656f3a5b
kapilkarda <kkarda77@gmail.com>
2022-12-23 16:50:18 +0530

    changed image url in email templates

--- src/templates/contactUs.js
+++ src/templates/contactUs.js
@@ -20,7 +20,7 @@ module.exports.contactUs = function (email, fullName, subject, message, address)
         <tr>
           <td>
             <a href="http://143.198.102.134:8080/" title="logo" target="_blank">
-              <img src="http://143.198.102.134:3003/companyLogo/hiriselogo1.png" alt="logo" 
+              <img src="http://143.198.102.134:3003/hiriselogo.png" alt="logo" 
               style=" width: 120px;">
             </a>
           </td>
