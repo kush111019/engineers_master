cb379e67b8fd6630dc229a4f8fd52247e76c8eeb
kapilkarda <kkarda77@gmail.com>
2023-01-26 11:42:02 +0530

    chnages in query to get  lead assigned to any user

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -729,7 +729,7 @@ const db_sql = {
               INNER JOIN
                 lead_industries AS i ON i.id = l.industry_type
               WHERE 
-                l.user_id = '{var1}' AND l.deleted_at IS NULL AND u.deleted_at IS NULL 
+                (l.user_id = '{var1}' OR l.assigned_sales_lead_to = 'var1') AND l.deleted_at IS NULL AND u.deleted_at IS NULL 
               ORDER BY 
                 l.created_at DESC`,
     
