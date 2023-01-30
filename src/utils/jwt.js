902be00e595c00ca9ac21650c4f60f9fb43206ff
kapilkarda <kkarda77@gmail.com>
2023-01-30 18:10:39 +0530

    added session expiry in middleware if company is locked by super admin

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,36 +1,61 @@
 const jsonwebtoken = require("jsonwebtoken");
+const { db_sql, dbScript } = require('../utils/db_scripts');
+const connection = require('../database/connection')
 
 const jwt = {
     //create token
     issueJWT: async user => {
-        let payload = {    
-        id : user.id, 
-        email : user.email  
+        let payload = {
+            id: user.id,
+            email: user.email
         };
         const options = {
-        expiresIn: '1d'
+            expiresIn: '1d'
         };
         const jwtToken = await jsonwebtoken.sign(payload, 'KEy', options)
         return jwtToken;
     },
     //verify Token 
     verifyTokenFn: async (req, res, next) => {
-      var token=req.headers.authorization                     
-        await jsonwebtoken.verify(token, 'KEy', function(err, decoded)
-        {                     
-            if (err) {                    
+        var token = req.headers.authorization
+        await jsonwebtoken.verify(token, 'KEy', function (err, decoded) {
+            if (err) {
                 return res.status(401).json({
                     success: false,
                     message: "Session timed out. Please sign in again",
                 });
-            }   else {                          
+            } else {
                 req.user = {
-                    id: decoded.id,  
-                    email: decoded.email,               
+                    id: decoded.id,
+                    email: decoded.email,
                 }
-                return next();
+
+                const checkLocked = async (id) => {
+                    try {
+                        let s1 = dbScript(db_sql['Q8'], { var1: id });
+                        let findUser = await connection.query(s1);
+                        let s2 = dbScript(db_sql['Q9'], { var1: findUser.rows[0].company_id });
+                        let checkCompany = await connection.query(s2);
+                        return !checkCompany.rows[0].is_locked;
+                    } catch (error) {
+                        console.error(error);
+                        return false;
+                    }
+                };
+                (async () => {
+                    let check = await checkLocked(req.user.id);
+                    if (!check) {
+                        return res.status(401).json({
+                            success: false,
+                            message: "Session timed out. Please sign in again",
+                        });
+                    } else {
+                        return next();
+                    }
+                })();
             }
         });
     }
 };
 module.exports = jwt;
+
