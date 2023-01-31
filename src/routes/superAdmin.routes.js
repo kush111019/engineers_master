519f614e967d5c3b48d6145d84e117c010e0ac87
kapilkarda <kkarda77@gmail.com>
2023-01-31 10:48:15 +0530

    fixed issue in superadmin login

--- src/routes/superAdmin.routes.js
+++ src/routes/superAdmin.routes.js
@@ -1,47 +1,47 @@
 const express = require('express')
 var router = express.Router()
 var controller = require('../controllers/index')
-const { verifyTokenFn } = require('../utils/jwt')
+const { verifyTokenFn1 } = require('../utils/jwt')
 const {uploadLogo} = require('../utils/uploadfiles')
 
 router.post('/login',controller.superAdmin.login)
 router.post('/forgetPassword', controller.superAdmin.forgotPassword)
 router.post('/resetPassword', controller.superAdmin.resetPassword)
 
-router.get('/showProfile',verifyTokenFn, controller.superAdmin.showProfile)
-router.get('/companiesList',verifyTokenFn,controller.superAdmin.companiesList)
-router.get('/showUsersByCompanyId',verifyTokenFn,controller.superAdmin.showUsersByCompanyId)
-router.get('/userWiseCompanyRevenue', verifyTokenFn, controller.superAdmin.userWiseCompanyRevenue)
-router.get('/dashboard', verifyTokenFn, controller.superAdmin.dashboard)
-router.get('/totalExpectedRevenueCounts', verifyTokenFn, controller.superAdmin.totalExpectedRevenueCounts)
-router.put('/lockOrUnlockCompany', verifyTokenFn, controller.superAdmin.lockOrUnlockCompany)
+router.get('/showProfile',verifyTokenFn1, controller.superAdmin.showProfile)
+router.get('/companiesList',verifyTokenFn1,controller.superAdmin.companiesList)
+router.get('/showUsersByCompanyId',verifyTokenFn1,controller.superAdmin.showUsersByCompanyId)
+router.get('/userWiseCompanyRevenue', verifyTokenFn1, controller.superAdmin.userWiseCompanyRevenue)
+router.get('/dashboard', verifyTokenFn1, controller.superAdmin.dashboard)
+router.get('/totalExpectedRevenueCounts', verifyTokenFn1, controller.superAdmin.totalExpectedRevenueCounts)
+router.put('/lockOrUnlockCompany', verifyTokenFn1, controller.superAdmin.lockOrUnlockCompany)
 //------------------------------------------plans----------------------------------------
 
-router.post('/addPlan', verifyTokenFn, controller.superAdmin.addPlan)
-router.get('/plansList', verifyTokenFn, controller.superAdmin.plansList)
-router.put('/updatePlan', verifyTokenFn, controller.superAdmin.updatePlan)
-router.put('/activateOrDeactivatePlan', verifyTokenFn, controller.superAdmin.activateOrDeactivatePlan)
+router.post('/addPlan', verifyTokenFn1, controller.superAdmin.addPlan)
+router.get('/plansList', verifyTokenFn1, controller.superAdmin.plansList)
+router.put('/updatePlan', verifyTokenFn1, controller.superAdmin.updatePlan)
+router.put('/activateOrDeactivatePlan', verifyTokenFn1, controller.superAdmin.activateOrDeactivatePlan)
 
 //-------------------------------Config-----------------------------------
-router.post('/addConfig', verifyTokenFn, controller.superAdmin.addConfig)
-router.get('/configList', verifyTokenFn, controller.superAdmin.configList)
+router.post('/addConfig', verifyTokenFn1, controller.superAdmin.addConfig)
+router.get('/configList', verifyTokenFn1, controller.superAdmin.configList)
 
 //----------------------------------------------------------------------------
-router.get('/subcribedCompaniesList', verifyTokenFn,controller.superAdmin.subcribedCompaniesList)
-router.get('/trialCompaniesList', verifyTokenFn,controller.superAdmin.trialCompaniesList)
-router.get('/allTrialAndSubcribedCompaniesList', verifyTokenFn,controller.superAdmin.allTrialAndSubcribedCompaniesList)
+router.get('/subcribedCompaniesList', verifyTokenFn1,controller.superAdmin.subcribedCompaniesList)
+router.get('/trialCompaniesList', verifyTokenFn1,controller.superAdmin.trialCompaniesList)
+router.get('/allTrialAndSubcribedCompaniesList', verifyTokenFn1,controller.superAdmin.allTrialAndSubcribedCompaniesList)
 
-router.get('/activeAndCanceledCompanies', verifyTokenFn , controller.superAdmin.activeAndCanceledCompanies)
-router.get('/planwiseCompaniesList/:planId', verifyTokenFn, controller.superAdmin.planwiseCompaniesList)
-router.put('/extendExpiryByCompanyId/:companyId', verifyTokenFn, controller.superAdmin.extendExpiryByCompanyId)
+router.get('/activeAndCanceledCompanies', verifyTokenFn1 , controller.superAdmin.activeAndCanceledCompanies)
+router.get('/planwiseCompaniesList/:planId', verifyTokenFn1, controller.superAdmin.planwiseCompaniesList)
+router.put('/extendExpiryByCompanyId/:companyId', verifyTokenFn1, controller.superAdmin.extendExpiryByCompanyId)
 
 //-----------------------------Enable/disable Imap service--------------------
-router.put('/enableDisableImapService', verifyTokenFn, controller.superAdmin.enableDisableImapService)
+router.put('/enableDisableImapService', verifyTokenFn1, controller.superAdmin.enableDisableImapService)
 
 //-----------------------------Enable/disable marketing service--------------------
-router.put('/enableDisableMarketingService', verifyTokenFn, controller.superAdmin.enableDisableMarketingService)
+router.put('/enableDisableMarketingService', verifyTokenFn1, controller.superAdmin.enableDisableMarketingService)
 
 //----------------------------Contact us Queries List-------------------------
-router.get('/contactUsQueriesList', verifyTokenFn,controller.superAdmin.contactUsQueriesList)
+router.get('/contactUsQueriesList', verifyTokenFn1,controller.superAdmin.contactUsQueriesList)
 
 module.exports = router;
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -55,6 +55,24 @@ const jwt = {
                 })();
             }
         });
+    },
+
+    verifyTokenFn1: async (req, res, next) => {
+        var token = req.headers.authorization
+        await jsonwebtoken.verify(token, 'KEy', function (err, decoded) {
+            if (err) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Session timed out. Please sign in again",
+                });
+            } else {
+                req.user = {
+                    id: decoded.id,
+                    email: decoded.email,
+                }
+                return next();
+            }
+        });
     }
 };
 module.exports = jwt;
