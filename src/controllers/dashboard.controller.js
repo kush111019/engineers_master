12807922089af760e2377d5a6e51b1942c9395b6
kapilkarda <kkarda77@gmail.com>
2023-01-31 06:22:34 -0500

    fixed dashboard issue

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,11 +8,13 @@ module.exports.revenues = async (req, res) => {
     try {
         let userId = req.user.id
         let { page, startDate, endDate, orderBy } = req.query
+        startDate = new Date(startDate).toISOString()
+        endDate = new Date(endDate).toISOString()
+        console.log("startDate",startDate,"endDate",endDate);
         let s3 = dbScript(db_sql['Q41'], { var1: moduleName, var2: userId })
         let checkPermission = await connection.query(s3)
         if (checkPermission.rows[0].permission_to_view_global) {
             let revenueCommissionBydate = []
-
             let s4 = dbScript(db_sql['Q87'], { var1: checkPermission.rows[0].company_id, var2: orderBy, var3: startDate, var4: endDate })
             let salesData = await connection.query(s4)
             if (salesData.rowCount > 0) {
