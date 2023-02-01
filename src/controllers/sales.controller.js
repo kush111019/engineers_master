b866ea9ef2bcd2f1caddc497d0c1b7a213df0b19
kapilkarda <kkarda77@gmail.com>
2023-02-01 20:41:12 +0530

    sales list issue

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -1045,7 +1045,35 @@ module.exports.closedSalesCommissionList = async (req, res) => {
                 for (data of salesCommissionList.rows) {
                     let closer = {}
                     let supporters = []
-
+                    let commission = 0
+                    if(data.slab_id){
+                        let s6 = dbScript(db_sql['Q184'],{var1 : data.slab_id})
+                        let slabData = await connection.query(s6)
+                        slabName = slabData.rows[0].slab_name;
+                        //if remainning amount is 0 then no reason to check 
+                        for (let i = 0; i < slabData.rows.length && remainingAmount > 0; i++) {
+                            let slab_percentage = Number(slabData.rows[i].percentage)
+                            let slab_maxAmount = Number(slabData.rows[i].max_amount)
+                            let slab_minAmount = Number(slabData.rows[i].min_amount)
+                            if (slabData.rows[i].is_max) {
+                                // Reached the last slab
+                                commission += ((slab_percentage / 100) * remainingAmount)
+                                break;
+                            }
+                            else {
+                                // This is not the last slab
+                                let diff = slab_minAmount == 0 ? 0 : 1
+                                let slab_diff = (slab_maxAmount - slab_minAmount + diff)
+                                slab_diff = (slab_diff > remainingAmount) ? remainingAmount : slab_diff
+                                commission += ((slab_percentage / 100) * slab_diff)
+                                remainingAmount -= slab_diff
+                                if (remainingAmount <= 0) {
+                                    break;
+                                }
+                            }
+                        }
+    
+                    }
                     let s4 = dbScript(db_sql['Q59'], { var1: data.id })
                     let supporter = await connection.query(s4)
                     if (supporter.rowCount > 0) {
