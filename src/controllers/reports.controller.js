494d59694be2097ab88a65963f8b74234edf9e5b
kapilkarda <kkarda77@gmail.com>
2023-02-01 18:56:38 +0530

    added console

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -301,6 +301,7 @@ module.exports.revenuePerSalesRep = async (req, res) => {
                         }
                     }
                 }
+                console.log(roleUsers,"roleUsers");
                 for(let id of roleUsers){
                     let s4 = dbScript(db_sql['Q258'], { var1: id, var2: orderBy, var3: limit, var4: offset, var5: sDate, var6: eDate })
                     let salesData = await connection.query(s4)
