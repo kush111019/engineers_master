d481d94ef7deaa7062b319d8c989cd3a4072e13b
kapilkarda <kkarda77@gmail.com>
2023-02-01 12:53:02 +0530

    fixed issue in lead

--- src/controllers/merketingStrategy.controller.js
+++ src/controllers/merketingStrategy.controller.js
@@ -172,10 +172,14 @@ module.exports.leadsList = async (req, res) => {
             let leadList = await connection.query(s2)
             if (leadList.rowCount > 0) {
                 for(let lead of leadList.rows){
-                    if(lead.assigned_sales_lead_to != "" || lead.assigned_sales_lead_to != null || lead.assigned_sales_lead_to != undefined){
+                    if(lead.assigned_sales_lead_to !== "" ){
                         let s3 = dbScript(db_sql['Q8'],{var1 : lead.assigned_sales_lead_to})
                         assignedSalesLead = await connection.query(s3)
-                        lead.assignedSalesLeadName = assignedSalesLead.rows[0].full_name
+                        if(assignedSalesLead.rowCount > 0){
+                            lead.assignedSalesLeadName = assignedSalesLead.rows[0].full_name
+                        }else{
+                            lead.assignedSalesLeadName = ''
+                        }
                     }else{
                         lead.assignedSalesLeadName = ''
                     }
