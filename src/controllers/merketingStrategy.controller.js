264da0e16a42a06d0899c363908fa3a8083def69
kapilkarda <kkarda77@gmail.com>
2023-02-01 12:44:07 +0530

    added console to debug

--- src/controllers/merketingStrategy.controller.js
+++ src/controllers/merketingStrategy.controller.js
@@ -233,9 +233,12 @@ module.exports.leadsList = async (req, res) => {
             }
             if (leadList.length > 0) {
                 for(let lead of leadList){
+                    console.log(lead,"lead");
                     if(lead.assigned_sales_lead_to != "" || lead.assigned_sales_lead_to != null || lead.assigned_sales_lead_to != undefined){
                         let s3 = dbScript(db_sql['Q8'],{var1 : lead.assigned_sales_lead_to})
+                        console.log(s3,"s3");
                         assignedSalesLead = await connection.query(s3)
+                        console.log(assignedSalesLead.rows, "assignedSalesLead");
                         lead.assignedSalesLeadName = assignedSalesLead.rows[0].full_name
                     }else{
                         lead.assignedSalesLeadName = ''
