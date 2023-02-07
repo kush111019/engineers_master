0a271766b36be3f1972c1c6db1575d638843e9ae
kapilkarda <kkarda77@gmail.com>
2023-02-07 12:04:54 +0530

    add console

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -120,24 +120,22 @@ module.exports.revenuePerCustomer = async (req, res) => {
                             let s3 = dbScript(db_sql['Q273'],{var1 : data.sales_commission_id})
                             let recognizedRevenue = await connection.query(s3)
                             if(recognizedRevenue.rowCount > 0){
-                                let obj = {
+                                console.log(data.customer_name,'data.customer_name')
+                                revenuePerCustomerArr.push({
                                     customer_name : data.customer_name || "",
                                     revenue : recognizedRevenue.rows[0].recognized_amount
-    
-                                }
-                                console.log(obj,'obj')
-                                revenuePerCustomerArr.push(obj)
+                                })
+                                console.log(revenuePerCustomerArr,'revenuePerCustomerArr')
                             }
                         }else{
                             let s4 = dbScript(db_sql['Q274'],{var1 : data.sales_commission_id})
                             let recognizedRevenue = await connection.query(s4)
                             if(recognizedRevenue.rowCount > 0){
-                                let obj = {
+                                revenuePerCustomerArr.push({
                                     customer_name : data.customer_name || "",
                                     revenue : recognizedRevenue.rows[0].recognized_amount
     
-                                }
-                                revenuePerCustomerArr.push(obj)
+                                })
                             }
                         } 
                     }
