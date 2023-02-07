c137e3b46c041aa4f9f9fff9b6dda7e6331ade15
kapilkarda <kkarda77@gmail.com>
2023-02-07 14:10:40 +0530

    update query in active sale api

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -634,11 +634,10 @@ const db_sql = {
               INNER JOIN customers AS cus ON cus.id = sc.customer_id
               WHERE sc.company_id = '{var1}' AND sc.deleted_at IS NULL and sc.closed_at IS NOT NULL  ORDER BY sc.created_at desc`,
     "Q181"  :`SELECT distinct(sc.id), sc.customer_id, sc.customer_commission_split_id, sc.is_overwrite,sc.business_contact_id, 
-              sc.revenue_contact_id,sc.qualification, sc.is_qualified, sc.target_amount, sc.currency, sc.target_closing_date, 
+              sc.revenue_contact_id,sc.qualification, sc.is_qualified, sc.target_amount, sc.currency, sc.target_closing_date,sc.transfered_back_by, 
               sc.sales_type, sc.subscription_plan,sc.recurring_date,sc.contract,sc.transfer_reason, sc.created_at,sc.user_id, sc.closed_at,sc.slab_id,sc.lead_id,
               c.closer_id, c.closer_percentage, u.full_name, u.email_address, cus.customer_name, cus.user_id as creater_id, u1.full_name AS creator_name,
               sup.supporter_id, sup.supporter_percentage,u2.email_address as supporter_email 
-              sc.transfered_back_by
               FROM sales_commission AS sc 
               INNER JOIN sales_closer AS c ON sc.id = c.sales_commission_id
               INNER JOIN sales_supporter AS sup ON sc.id = sup.sales_commission_id
