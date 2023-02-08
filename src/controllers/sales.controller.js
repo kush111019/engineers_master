2c9df5a8ca340661b0bec86c7046280cdf2dd1f5
kapilkarda <kkarda77@gmail.com>
2023-02-08 17:51:34 +0530

    console added

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -443,8 +443,10 @@ module.exports.allSalesCommissionList = async (req, res) => {
         } else if (checkPermission.rows[0].permission_to_view_own) {
             let salesListArr = []
             let roleUsers = await getUserAndSubUser(checkPermission.rows[0]);
+            console.log(roleUsers,'roleUsers')
             for (let id of roleUsers) {
                 let s3 = dbScript(db_sql['Q178'], { var1: id.slice(1,-1) })
+                console.log(s3,'s3')
                 let salesCommissionList = await connection.query(s3)
                 for (let data of salesCommissionList.rows) {
                     let closer = {}
