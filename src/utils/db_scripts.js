70495aff750b756abdbd4a526a65464268611e88
kapilkarda <kkarda77@gmail.com>
2023-02-13 15:12:15 +0530

    changes in query

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -1589,7 +1589,7 @@ const db_sql = {
                 FROM 
                   lead_organizations AS o
                 LEFT JOIN users AS u ON u.id = o.user_id
-                LEFT JOIN customers as c ON c.id = o.id
+                LEFT JOIN customers as c ON c.organization_id  = o.id
                 WHERE o.company_id = '{var1}' AND o.deleted_at IS NULL AND u.deleted_at IS NULL 
                 ORDER BY 
                   o.created_at DESC`,
