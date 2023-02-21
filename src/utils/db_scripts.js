8558fbd2ce93d020143ad36cf44c462b002130e3
kapilkarda <kkarda77@gmail.com>
2023-02-21 10:11:09 +0530

    change forcast query

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -2075,7 +2075,7 @@ const db_sql = {
                 (
                   SELECT json_agg(fa.*)
                     from forecast_audit fa
-                  WHERE (fa.forecast_id::uuid = f.id OR fa.pid::uuid = f.id)
+                  WHERE (fa.forecast_id::uuid = f.id OR fa.forecast_id::uuid = f.pid::uuid)
                 ) as audit_forecast
               FROM 
                 forecast AS f
