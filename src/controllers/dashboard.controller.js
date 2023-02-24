d5470835e1c19a6d5ba1b1833be930929d48d4a6
kapilkarda <kkarda77@gmail.com>
2023-02-24 17:37:14 +0530

    remove console in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -21,7 +21,6 @@ module.exports.revenues = async (req, res) => {
             let roleUsers = await getUserAndSubUser(checkPermission.rows[0]);
             let s4 = dbScript(db_sql['Q87'], { var1: checkPermission.rows[0].company_id, var2: orderBy, var3: sDate, var4: eDate, var5: roleUsers.join(',') })
             let salesData = await connection.query(s4)
-            console.log(salesData.rows);
             if (salesData.rowCount > 0) {
                 for (let saleData of salesData.rows) {
 
@@ -33,7 +32,6 @@ module.exports.revenues = async (req, res) => {
                     }else{
                         let s4 = dbScript(db_sql['Q295'], { var1: saleData.sales_commission_id })
                         let salesSubscriptionData = await connection.query(s4)
-                        console.log(salesSubscriptionData.rows,'subscription')
                         let subscriptionBooking = 0;
                         for( let subscription of salesSubscriptionData.rows){
                             if(Number(subscription.recognized_amount) <= Number(subscription.subscription_amount)){
@@ -46,12 +44,8 @@ module.exports.revenues = async (req, res) => {
                         revenueCommissionByDateObj.subscription_booking = Number(subscriptionBooking);
                     }
 
-
-                   
-                   
                     let s5 = dbScript(db_sql['Q300'], { var1: saleData.sales_commission_id })
                     let recognizedRevenueData = await connection.query(s5)
-                    console.log(recognizedRevenueData.rows,'recognizedRevenueData');
                     if (recognizedRevenueData.rows[0].amount) {
                         revenueCommissionByDateObj.revenue = Number(recognizedRevenueData.rows[0].amount)
                         revenueCommissionByDateObj.date = moment(saleData.closed_at).format('MM/DD/YYYY')
@@ -86,7 +80,6 @@ module.exports.revenues = async (req, res) => {
                     }
                 }
             }
-            console.log(revenueCommissionBydate)
             if (revenueCommissionBydate.length > 0) {
                 let returnData = await reduceArrayWithCommission(revenueCommissionBydate)
                 if (returnData.length > 0) {
@@ -124,7 +117,25 @@ module.exports.revenues = async (req, res) => {
             if (salesData.rowCount > 0) {
                 for (let saleData of salesData.rows) {
                     let revenueCommissionByDateObj = {}
-                    revenueCommissionByDateObj.booking = Number(saleData.target_amount);
+                    
+                    if(saleData.sales_type == 'Perpetual'){
+                        revenueCommissionByDateObj.booking = Number(saleData.target_amount);
+                        revenueCommissionByDateObj.subscription_booking = 0;
+                    }else{
+                        let s4 = dbScript(db_sql['Q295'], { var1: saleData.sales_commission_id })
+                        let salesSubscriptionData = await connection.query(s4)
+                        let subscriptionBooking = 0;
+                        for( let subscription of salesSubscriptionData.rows){
+                            if(Number(subscription.recognized_amount) <= Number(subscription.subscription_amount)){
+                                subscriptionBooking = Number(subscription.subscription_amount)
+                            }else{
+                                subscriptionBooking = Number(subscription.recognized_amount)
+                            }
+                        }
+                        revenueCommissionByDateObj.booking =0;
+                        revenueCommissionByDateObj.subscription_booking = Number(subscriptionBooking);
+                    }
+
                     let s5 = dbScript(db_sql['Q300'], { var1: saleData.sales_commission_id })
                     let recognizedRevenueData = await connection.query(s5)
 
@@ -150,7 +161,6 @@ module.exports.revenues = async (req, res) => {
                             let commission1 = 0;
                             for (let user of saleData.sales_users) {
                                 if (user.user_type == process.env.SUPPORT) {
-                                    console.log(user)
                                     booking_commission1 = booking_commission1 + ((Number(user.percentage) / 100) * Number(saleData.booking_commission));
                                     commission1 = commission1 + ((Number(user.percentage) / 100) * Number(commission))
 
@@ -340,15 +350,12 @@ module.exports.totalExpectedRevenueCounts = async (req, res) => {
 module.exports.dataCreationStatus = async (req, res) => {
     try {
         let userId = req.user.id
-        console.log(userId, '')
         let s3 = dbScript(db_sql['Q41'], { var1: moduleName, var2: userId })
         let checkPermission = await connection.query(s3)
         if (checkPermission.rowCount > 0) {
 
-            console.log(checkPermission.rows)
             let s4 = dbScript(db_sql['Q9'], { var1: checkPermission.rows[0].company_id })
             let companyData = await connection.query(s4)
-            console.log(companyData.rows)
 
             if (companyData.rowCount > 0) {
                 res.json({
