b5538b3f92c7cf6f9417ebca4cb5b2c45aad5035
kapilkarda <kkarda77@gmail.com>
2023-02-27 10:42:46 +0530

    add console

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -21,6 +21,7 @@ module.exports.revenues = async (req, res) => {
             let roleUsers = await getUserAndSubUser(checkPermission.rows[0]);
             let s4 = dbScript(db_sql['Q87'], { var1: checkPermission.rows[0].company_id, var2: orderBy, var3: sDate, var4: eDate, var5: roleUsers.join(',') })
             let salesData = await connection.query(s4)
+            console.log(salesData.rows,'sales data')
             if (salesData.rowCount > 0) {
                 for (let saleData of salesData.rows) {
 
