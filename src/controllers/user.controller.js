806716ea06c1b9d2316180cdbb15b0cf9f9d5046
kapilkarda <kkarda77@gmail.com>
2023-03-21 18:56:50 +0530

    fixed issue in user list

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -297,9 +297,9 @@ module.exports.usersList = async (req, res) => {
     try {
         let userId = req.user.id
         let status = "all"
-        
+
         if(req?.query?.status) {
-            status  = req?.query?.status;
+            status  = (req?.query?.status == 'undefined' || req?.query?.status == undefined ) ? 'all' : req?.query?.status;
         }
         // here we are getting user permission's
         let s3 = dbScript(db_sql['Q41'], { var1: moduleName, var2: userId })
