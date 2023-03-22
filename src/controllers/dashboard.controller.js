5281c41461d020d7a2083c1ae187bb4b22838368
kapilkarda <kkarda77@gmail.com>
2023-03-22 10:30:46 +0530

    added consoles in dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -115,8 +115,12 @@ module.exports.revenues = async (req, res) => {
                     } else {
                         let s4 = dbScript(db_sql['Q252'], { var1: saleData.sales_commission_id })
                         let salesSubscriptionData = await connection.query(s4)
+                        console.log(salesSubscriptionData.rows,"salesSubscriptionData1111");
                         let subscriptionBooking1 = 0;
                         if (salesSubscriptionData.rowCount > 0) {
+                            console.log(salesSubscriptionData.rows[0],"salesSubscriptionData.rows[0]22222");
+                            console.log(salesSubscriptionData.rows[0].archived_at,"salesSubscriptionData.rows[0].archived_at");
+                            console.log(salesSubscriptionData.rows[0].subscription_amount,"salesSubscriptionData.rows[0].subscription_amount");
                             let s5 = dbScript(db_sql['Q256'], { var1: saleData.sales_commission_id })
                             let recognizedRevenueData = await connection.query(s5)
                             if (recognizedRevenueData.row[0].amount) {
