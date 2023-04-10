54ff455b6e6483ac0829d502d50c502c3cbdea41
kapilkarda <kkarda77@gmail.com>
2023-04-10 12:21:06 +0530

    fixed issue in update sales

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -485,11 +485,11 @@ module.exports.updateSales = async (req, res) => {
             let findSales = await connection.query(s11)
             let committedDate = (!is_qualified) ? 'null' :
                 (findSales.rows[0].committed_at && is_qualified) ? findSales.rows[0].committed_at :
-                (!findSales.rows[0].committed_at && is_qualified) ? new Date().toISOString() : 'null';
+                (!findSales.rows[0].committed_at && is_qualified) ? _dt : 'null';
 
             let performedDate =  (!is_service_performed) ? 'null' :
             (findSales.rows[0].service_performed_at && is_service_performed) ? findSales.rows[0].service_performed_at :
-            (!findSales.rows[0].service_performed_at && is_service_performed) ? new Date().toISOString() : 'null';
+            (!findSales.rows[0].service_performed_at && is_service_performed) ? _dt : 'null';
 
             totalCommission = totalCommission + commission
             let s5 = dbScript(db_sql['Q62'], { var1: customerId, var2: commissionSplitId, var3: is_overwrite, var4: _dt, var5: salesId, var6: checkPermission.rows[0].company_id, var7: businessId, var8: revenueId, var9: mysql_real_escape_string(qualification), var10: is_qualified, var11: targetAmount, var12: targetClosingDate, var14: salesType, var15: subscriptionPlan, var16: recurringDate, var17: currency, var18: slabId, var19: leadId, var20: totalCommission, var21: committedDate, var22: is_service_performed, var23 : mysql_real_escape_string(service_perform_note), var24 : performedDate  })
