32d46a54e29ff4fb4417eb78b5880fb54d719036
kapilkarda <kkarda77@gmail.com>
2023-04-27 17:22:24 +0530

    changes in sales details

--- src/controllers/proUsers.controller.js
+++ src/controllers/proUsers.controller.js
@@ -2427,7 +2427,7 @@ module.exports.captainWiseSalesDetails = async (req, res) => {
     
                     let updatedSalesDetails = salesDetails.rows.map((sale, index) => ({
                         ...sale,
-                        ...notesCount.rows[index]
+                        ...(notesCount.rows[index] ? { notes_count: notesCount.rows[index] } : { notes_count: 0 })
                     }));
 
                     let count = 0
@@ -2452,7 +2452,7 @@ module.exports.captainWiseSalesDetails = async (req, res) => {
                         avgNotesCount: avgNotesCount,
                         maxNotesCount: maxNotesCount,
                         minNotesCount: minNotesCount,
-                        sciiCount: sciiCount
+                        scii: sciiCount
                     }
                 }else{
                     captainWiseSaleObj = {
