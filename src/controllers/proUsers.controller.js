cb06a4544d619a71d10dd0fe8aff10f90695807e
kapilkarda <kkarda77@gmail.com>
2023-06-21 22:14:54 +0530

    made changes in response

--- src/controllers/proUsers.controller.js
+++ src/controllers/proUsers.controller.js
@@ -3215,8 +3215,8 @@ module.exports.salesMetricsReport = async (req, res) => {
                         findMissingRR.high_risk_total_missing_rr = totalHighRiskRR;
                         findMissingRR.low_risk_total_missing_rr = totalLowRiskRR;
                     } else {
-                        findMissingRR.high_risk_missing_rr = "";
-                        findMissingRR.low_risk_missing_rr = "";
+                        findMissingRR.high_risk_missing_rr = [];
+                        findMissingRR.low_risk_missing_rr = [];
                         findMissingRR.high_risk_total_missing_rr = 0
                         findMissingRR.low_risk_total_missing_rr = 0;
                     }
