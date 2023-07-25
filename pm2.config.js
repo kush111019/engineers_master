511d2d2123fde5135fc6dbce8af088373e10281f
kapilkarda <kkarda77@gmail.com>
2023-07-25 12:07:10 +0530

    created pm2 config file and changes un index.js file

---
+++ pm2.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+      {
+        name: "Express App",
+        script: "index.js",
+        instances: "max",
+        autorestart: true,
+        watch: true,
+        max_memory_restart: "1G",
+        env: {
+          NODE_ENV: "development"
+        },
+        env_production: {
+          NODE_ENV: "production"
+        }
+      }
+    ]
+  };
+  
