747ad80a09523fb1c4eb439e9fc14e0958d3dc4e
kapilkarda <kkarda77@gmail.com>
2023-11-06 11:27:29 +0530

    update migration code

--- migration/update_sales_product_logs.js
+++ migration/update_sales_product_logs.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const uuid = require("node-uuid");
 require("dotenv").config();
 
-var connection = new Pool({
+const pool = new Pool({
   host: process.env.HOST,
   user: process.env.DATABASE_USER,
   password: process.env.PASSWORD,
@@ -10,20 +10,28 @@ var connection = new Pool({
   charset: "utf8mb4",
 });
 
-connection.query(
-  `DO $$
-  BEGIN
-      IF NOT EXISTS (SELECT column_name FROM information_schema.columns WHERE table_name = 'follow_up_notes' AND column_name = 'notes_type') THEN
-          ALTER TABLE follow_up_notes
-          ADD COLUMN notes_type VARCHAR(255) DEFAULT '1';
-      END IF;
-  END $$;`,
-  (err) => {
-    if (err) {
-      throw err;
-    }
-  }
-);
-
-connection.end();
-process.exit();
+pool.connect()
+  .then(client => {
+    return client.query(
+      `DO $$
+      BEGIN
+          IF NOT EXISTS (SELECT column_name FROM information_schema.columns WHERE table_name = 'follow_up_notes' AND column_name = 'notes_type') THEN
+              ALTER TABLE follow_up_notes
+              ADD COLUMN notes_type VARCHAR(255) DEFAULT '1';
+          END IF;
+      END $$;`
+    )
+    .then(res => {
+      console.log("Column added successfully:", res);
+      client.release(); // Release the client back to the pool
+      process.exit();
+    })
+    .catch(err => {
+      console.error("Error adding column:", err);
+      client.release(); // Release the client back to the pool
+      process.exit();
+    });
+  })
+  .catch(err => {
+    console.error("Error connecting to the database:", err);
+  });
