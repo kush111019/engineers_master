c79f40faa43cad85c424855c0b87a71267915f05
kapilkarda <kkarda77@gmail.com>
2023-11-08 18:30:37 +0530

    Update the Query

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -4801,13 +4801,24 @@ GROUP BY
     GROUP BY sc.id, cus.customer_name, u1.full_name, u1.email_address, slab.slab_name, u2.full_name, cus.user_id
     ORDER BY sc.created_at DESC;
     `,
-  "Q475": `SELECT DISTINCT s.id, ce.full_name, c.customer_name, ce.title, fn.notes, fn.created_at
-            FROM product_in_sales ps
-            JOIN follow_up_notes fn ON ps.sales_id = fn.sales_id 
-            JOIN sales s ON s.id = ps.sales_id
-            JOIN customer_company_employees ce ON ce.id = s.lead_id
-            JOIN customer_companies c ON ce.customer_company_id=c.id
-            WHERE fn.notes_type LIKE '%2%' and ps.product_id='{var1}';`
+  // "Q475": `SELECT DISTINCT s.id, ce.full_name, c.customer_name, ce.title, fn.notes, fn.created_at
+  //           FROM product_in_sales ps
+  //           JOIN follow_up_notes fn ON ps.sales_id = fn.sales_id 
+  //           JOIN sales s ON s.id = ps.sales_id
+  //           JOIN customer_company_employees ce ON ce.id = s.lead_id
+  //           JOIN customer_companies c ON ce.customer_company_id=c.id
+  //           WHERE fn.notes_type LIKE '%2%' and ps.product_id='{var1}';`
+  "Q475": `WITH relevant_sales AS (
+              SELECT DISTINCT ps.sales_id
+              FROM product_in_sales ps
+              WHERE ps.product_id = '{var1}'
+          )
+          SELECT s.id, ce.full_name, c.customer_name, ce.title, fn.notes, fn.created_at
+          FROM sales s
+          JOIN customer_company_employees ce ON ce.id = s.lead_id
+          JOIN customer_companies c ON c.id = ce.customer_company_id
+          JOIN relevant_sales rs ON rs.sales_id = s.id
+          JOIN follow_up_notes fn ON fn.sales_id = rs.sales_id AND fn.notes_type LIKE '%2';`
 }
   ;
 
