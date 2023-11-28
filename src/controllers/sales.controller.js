70e78dad248f8822f9519d915ec4028504361d2a
kapilkarda <kkarda77@gmail.com>
2023-11-28 13:56:30 +0530

    added more console

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -168,7 +168,7 @@ module.exports.createSales = async (req, res) => {
 
                 console.log("products ", products);
                 for(let pid of products) {
-                    console.log("pid ", pid);
+                    console.log("pid ", leadId, createSales.rows[0].id. userId, "Sales Created", checkPermission.rows[0].company_id, pid);
                     await LeadActivityCreate(leadId, createSales.rows[0].id. userId, "Sales Created", checkPermission.rows[0].company_id, pid);
                 }
 
