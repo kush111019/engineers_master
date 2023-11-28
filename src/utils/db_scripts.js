a2756af77d889795773389fe6d715b6a444a839a
kapilkarda <kkarda77@gmail.com>
2023-11-28 11:50:55 +0530

    update insert query

--- src/utils/db_scripts.js
+++ src/utils/db_scripts.js
@@ -68,7 +68,7 @@ const db_sql = {
   Q28: `UPDATE permissions SET deleted_at = '{var2}' WHERE role_id = '{var1}' AND deleted_at IS NULL RETURNING * `,
   Q29: `UPDATE slabs SET deleted_at = '{var1}' WHERE id = '{var2}' AND company_id = '{var3}' AND deleted_at IS NULL`,
   Q30: `UPDATE users SET is_locked = '{var1}', updated_at = '{var3}' WHERE company_id = '{var2}' AND is_main_admin = false AND deleted_at IS NULL RETURNING * `,
-  Q31: `INSERT INTO customer_company_employees_activities (sales_id, company_id, user_id, notes, notes_type, lead_id, product_id) VALUES('{var1}','{var2}','{var3}','{var4}','{var5}','{var6}', ,'{var7}') RETURNING *`,
+  Q31: `INSERT INTO customer_company_employees_activities (sales_id, company_id, user_id, notes, notes_type, lead_id, product_id) VALUES('{var1}','{var2}','{var3}','{var4}','{var5}','{var6}', '{var7}') RETURNING *`,
   Q32: `SELECT f.id, f.notes, f.notes_type, f.created_at, f.user_id, u.full_name, u.avatar, p.id as product_id, p.product_name
               FROM customer_company_employees_activities as f
               LEFT JOIN users AS u ON u.id = f.user_id
