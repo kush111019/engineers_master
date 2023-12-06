e707c0dd767b919717584abf903bdcd9a74b2d58
kapilkarda <kkarda77@gmail.com>
2023-12-06 15:18:16 +0530

    remove the console

--- src/controllers/proUsers.controller.js
+++ src/controllers/proUsers.controller.js
@@ -5070,7 +5070,6 @@ module.exports.salesMetricsReport = async (req, res) => {
             var2: SformattedDate,
             var3: EformattedDate,
           });
-          console.log("3.7 ", s12);
           let findEOLProduct = await connection.query(s12);
           if (findEOLProduct.rowCount > 0) {
             let findEOLSales = await calculateEOLProducts(findEOLProduct.rows);
